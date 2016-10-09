6fabe2a82053545ad3ef8c18c13253cc65b7ee7a
davidsichinava <david@sichinava.ge>
2016-10-08 20:01:37 -0600

    Finalized script for scraping proportional elections

--- scrGeo.R
+++ scrGeo.R
@@ -1,4 +1,5 @@
 ## Load necessary libraries
+library(tidyr)
 library(XML)
 library(rvest)
 library(plyr)
@@ -27,44 +28,31 @@ dfURL <- data.frame(urls = urls, stringsAsFactors = FALSE)
 dfURL<-as.data.frame(dfURL[grep("*olq*", dfURL$urls), ])
 names(dfURL)<-c("urls")
 
-dfURL<-(dfURL[grep("*olq*", dfURL$urls), ])
-dfURL<-as.data.frame(paste0("http://results.cec.gov.ge/", dfURL, sep=""))
-
 ## Loop thourgh URLs and get data tables for each majoritarian district
 ## Create an empty data frame for pasting precinct level results
 PropData <- as.data.frame(setNames(replicate(27,numeric(0), simplify = F), letters[1:27]))
 
 
 for(i in 1:nrow(dfURL)) {
-  scrURL <- GET(paste0("http://results.cec.gov.ge/", (dfURL$urls[i]), sep=""), user_agent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0")) %>% 
+  propRaw <- GET(paste0("http://results.cec.gov.ge/", (dfURL$urls[i]), sep=""), user_agent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0")) %>% 
   read_html() %>%    
   html_nodes(xpath='//*[(@id = "table36")]')%>%
   html_table(fill=TRUE, header=TRUE)
 propRaw<-as.data.frame(propRaw)
-PropData<-rbind(PropData, propRaw)
+PropData<-na.omit(rbind(PropData, propRaw))
 }
 
 names(PropData)<-c("PrecinctID", "Party_1", "Party_2", "Party_3", "Party_4", "Party_5", "Party_6", "Party_7", "Party_8", "Party_10", "Party_11", "Party_12", "Party_14", "Party_15", "Party_16", "Party_17", "Party_18", "Party_19", "Party_22", "Party_23", "Party_25", "Party_26", "Party_27", "Party_28", "Party_30", "Party_41", "Protocol")
 
-####
+## Construct proper IDs for precincts
+PropData<-PropData %>% separate(PrecinctID, c("MID", "DID", "PID"), sep = "\\.", remove=FALSE)
 
-for(i in 1:nrow(dfURL)) {
-  scrURL<-html_session(paste0("http://results.cec.gov.ge/", (dfURL$urls[i]), sep=""))
-  propRaw<-scrURL%>%
-    read_html() %>%    
-    html_nodes(xpath='//*[(@id = "table36")]')%>%
-    html_table(fill=TRUE, header=TRUE)
-  propRaw<-as.data.frame(propRaw)
-  PropData<-rbind(PropData, propRaw)
-}
+PropData$MID<-as.numeric(PropData$MID)
+PropData$DID<-as.numeric(PropData$DID)
+PropData$PID<-as.numeric(PropData$PID)
+PropData$Precid<-PropData$MID*100000+PropData$DID*1000+PropData$PID
+PropData<-PropData[,]
+names(PropData)<-c("PrecinctID", "MID", "DID", "PID", "Party_1", "Party_2", "Party_3", "Party_4", "Party_5", "Party_6", "Party_7", "Party_8", "Party_10", "Party_11", "Party_12", "Party_14", "Party_15", "Party_16", "Party_17", "Party_18", "Party_19", "Party_22", "Party_23", "Party_25", "Party_26", "Party_27", "Party_28", "Party_30", "Party_41", "Protocol", "PrecID")
 
-for(i in 1:nrow(dfURL)) {
-  scrURL<-html_session("http://results.cec.gov.ge/olq_2.html")
-  propRaw<-scrURL%>%
-    read_html() %>%    
-    html_nodes(xpath='//*[(@id = "table36")]')%>%
-    html_table(fill=TRUE, header=TRUE)
-  propRaw<-as.data.frame(propRaw)
-  PropData<-rbind(PropData, propRaw)
-  Sys.sleep(5)
-}
+write.csv(PropData, "PropData.csv")
+
