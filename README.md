38c8024b112f334b0a554d2d3fd073736a931581
David Sichinava <david@sichinava.ge>
2016-10-10 11:14:23 -0600

    Updated scripts for scraping majoritarian races

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # geoelections2016
-Scraping script for 2016 Georgia parliamentary elections form the [results](http://results.cec.gov.ge/) website of the Georgian CEC.
-
-Currently the scaper is written only for proportional results. Script will be updated as the CEC starts publishing the results of SMDs
+Scraping script for 2016 Georgia parliamentary elections
--- scrGeo.R
+++ scrGeo.R
@@ -12,6 +12,8 @@ library(data.table)
 library(stringr)
 library(foreach)
 library(curl)
+library(purrr)
+
 
 setwd("D:\\Dropbox\\My Projects\\Elections\\Geogia 2016 Parliamentary\\github\\geoelections2016")
 
@@ -46,13 +48,48 @@ names(PropData)<-c("PrecinctID", "Party_1", "Party_2", "Party_3", "Party_4", "Pa
 
 ## Construct proper IDs for precincts
 PropData<-PropData %>% separate(PrecinctID, c("MID", "DID", "PID"), sep = "\\.", remove=FALSE)
-
+PropData[is.na(PropData)] <- ""
 PropData$MID<-as.numeric(PropData$MID)
 PropData$DID<-as.numeric(PropData$DID)
 PropData$PID<-as.numeric(PropData$PID)
 PropData$Precid<-PropData$MID*100000+PropData$DID*1000+PropData$PID
-PropData<-PropData[,]
+
 names(PropData)<-c("PrecinctID", "MID", "DID", "PID", "Party_1", "Party_2", "Party_3", "Party_4", "Party_5", "Party_6", "Party_7", "Party_8", "Party_10", "Party_11", "Party_12", "Party_14", "Party_15", "Party_16", "Party_17", "Party_18", "Party_19", "Party_22", "Party_23", "Party_25", "Party_26", "Party_27", "Party_28", "Party_30", "Party_41", "Protocol", "PrecID")
 
-write.csv(PropData, "PropData.csv")
+write.csv(PropData, "PropData.csv", row.names=FALSE)
 
+### Majoritarian districts
+mData <- as.data.frame(setNames(replicate(30,numeric(0), simplify = F), letters[1:30]))
+
+for(i in seq_along(1:73)){
+  majRaw <- GET(paste0("http://results.cec.gov.ge/maj_", i, ".html", sep=""), user_agent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0")) %>% 
+    read_html() %>%    
+    html_nodes(xpath='//*[(@id = "table36")]')%>%
+    html_table(fill=TRUE, header=TRUE)
+    majRaw<-as.data.frame(majRaw)
+    colnames(majRaw)[1] <- "PrecinctID"
+    colnames(majRaw)[ncol(majRaw)] <- "Protocol"
+    mData<-bind_rows(mData, majRaw)
+}
+
+## Remove duplicates
+mData<-unique(mData)
+
+## Subset columns with data
+mData<-subset(mData,select=-c(a:NA..3))
+
+## Reorder columns
+mData<-mData[c("PrecinctID","X1","X2","X3","X4","X5","X6","X7","X8","X10","X12","X14","X15","X16","X17","X18","X19","X22","X23","X25","X26","X27","X28","X41","X42","X43","X44","Protocol")]
+
+
+## Generate proper precinct codes
+
+mData<-mData %>% separate(PrecinctID, c("MID", "DID", "PID"), sep = "\\.", remove=FALSE)
+mData$MID<-as.numeric(mData$MID)
+mData$DID<-as.numeric(mData$DID)
+mData$PID<-as.numeric(mData$PID)
+mData$Precid<-mData$MID*100000+mData$DID*1000+mData$PID
+mData[is.na(mData)] <- ""
+
+## Export to csv
+write.csv(mData, "mdata.csv", row.names=FALSE)
